60b0efbcc17aacf84681c89dabd77eab1c7e4e57
MariErsland <257123@uis.no>
2023-01-16 18:20:37 +0100

    Made fetch call with button

--- SupReactNative/App.tsx
+++ SupReactNative/App.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button, ScrollView } from 'react-native';
+
 
 const App = () => {
+
+function Fetch(){
+  const users = fetch("http://152.94.160.72:3000/users")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data)
+    setData(data);
+  })
+}
+
+const [data, setData] = useState([{id: 100, email: 1, first_name: "Mari"}]);
   return (
     <View>
-      <Text>Hello, React Native!</Text>
+        <Text>Hello, React Native!</Text>
+        <Button title="Press me" onPress={Fetch}/>
+        {
+          data.map((item) => {
+            return(
+              <View key={item.id}>
+                <Text>{item.first_name}</Text>
+              </View>
+            )
+          })
+        }
     </View>
   );
 };
