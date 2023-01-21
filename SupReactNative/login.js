7dc04e160c1e4631ad9486f2433667b62d42795a
MariErsland <257123@uis.no>
2023-01-21 11:52:00 +0100

    Removed stuff in loginpage

--- SupReactNative/login.js
+++ SupReactNative/login.js
@@ -1,33 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { View } from 'react-native';
-import GoogleSignin from 'react-native-google-signin';
 
 const LoginScreen = () => {
-  useEffect(() => {
-    GoogleSignin.configure({
-      webClientId: '678861880955-v3t20enq0fbkttjdqj7mf05s41ch5tu6.apps.googleusercontent.com',
-      offlineAccess: true,
-    });
-  }, []);
-
-  const handleGoogleSignIn = async () => {
-    try {
-      await GoogleSignin.hasPlayServices();
-      const userInfo = await GoogleSignin.signIn();
-      console.log(userInfo);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
+  
   return (
     <View>
-      <GoogleSigninButton
-        style={{ width: 192, height: 48 }}
-        size={GoogleSigninButton.Size.Wide}
-        color={GoogleSigninButton.Color.Dark}
-        onPress={handleGoogleSignIn}
-      />
+      
     </View>
   );
 };
