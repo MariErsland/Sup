b2ab8af3414f2d5060941778fd9b3ae110bc48f7
MariErsland <257123@uis.no>
2023-01-23 15:52:41 +0100

    Made login with google frontend only

--- SupReactNative/login.js
+++ SupReactNative/login.js
@@ -1,13 +1,56 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, Text, Button} from 'react-native';
+import {GoogleSignin, GoogleSigninButton} from '@react-native-google-signin/google-signin';
 
-const LoginScreen = () => {
-  
+GoogleSignin.configure({
+  webClientId:
+    '793626058046-1vvfcdoglco03l1aitub77m9u8dqbfld.apps.googleusercontent.com',
+  offlineAccess: false,
+});
+
+const Login = () => {
+
+  const [userInfo, setUserInfo] = useState(null);
+
+  const onSignIn = () => {
+    GoogleSignin.hasPlayServices()
+      .then(() => {
+        return GoogleSignin.signIn();
+      })
+      .then((response) => {
+        setUserInfo(response);
+      })
+      .catch((err) => {
+        console.log(err);
+    });
+  };
+
+  const onSignOut = () => {
+    GoogleSignin.signOut()
+      .then(() => {
+        setUserInfo(null);
+      })
+      .catch((err) => {
+      });
+  };
+
   return (
-    <View>
-      
-    </View>
+      <SafeAreaView>
+        <Text> 
+        {userInfo ? (
+          <>
+            <Text>
+              Hello  {`${userInfo.user.givenName} ${userInfo.user.familyName}`}
+            </Text>
+            <></>
+            <Button title="Sign out" onPress={onSignOut} />
+          </>
+        ) : (
+          <GoogleSigninButton onPress={onSignIn} />
+        )}
+        </Text>
+      </SafeAreaView>
   );
 };
 
-export default LoginScreen;
+export default Login;
--- SupReactNative/types.ts
+++ SupReactNative/types.ts
@@ -1,5 +1,6 @@
 export type RootStackParamList = {
     Feed: undefined;
     Profile: undefined;
+    Login: undefined; 
 }
 
