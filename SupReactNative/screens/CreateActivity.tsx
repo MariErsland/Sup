62afff53464db0d4cc0057b0954b918a84d87629
issi1000 <iselin.bjaanes@hotmail.com>
2023-02-03 09:30:28 +0100

    Fixed footer,dropdown,date/time,UI

--- SupReactNative/screens/CreateActivity.tsx
+++ SupReactNative/screens/CreateActivity.tsx
@@ -1,25 +1,55 @@
 import { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, ScrollView} from 'react-native';
+import { SelectList } from 'react-native-dropdown-select-list'
 import Footer from '../shared/Footer';
+import DatePicker from 'react-native-modern-datepicker';
+import React from 'react';
+
+interface NewActivity {
+    }
 
 const NewActivity = () => {
-    const [time, setTime] = useState('');
-    const [county, setCounty] = useState('');
+
+    const [selectedCategory, setSelectedCategory] = useState(null);
+    const [selectedCounty, setSelectedCounty] = useState(null);
+
+    const [selectedDate, setSelectedDate] = useState('');
+    console.log(selectedDate);
+
     const [address, setAddress] = useState('');
-    const [category, setCategory] = useState('');
+
+    const [showDatePicker, setShowDatePicker] = useState(false);
+    console.log(showDatePicker)
+
+
+    const [category] = useState([
+        { label: 'Friluft', value: 'Tur'},
+        { label: 'Trening', value: 'Jogging'},
+        { label: 'Matlaging', value: 'Matlaging'}, 
+        { label: 'Annet', value: 'Annet'}, 
+
+        ])
+
+  
+     const [county] = useState([
+        { label: 'Rogaland', value: 'Rogaland'},
+        { label: 'Agder', value: 'Agder'},
+        ])
+         
+
     const [description, setDescription] = useState('');
     const [number_of_participants, setNumberOfParticipants] = useState('');
     const [created_by, setCreatedBy] = useState('');
 
     const handleCreateActivity = async () => {
-        console.log(time, county, category);
-        console.log(JSON.stringify({ time, county, address, category, description, number_of_participants, created_by }));
+        console.log(selectedDate, county, category);
+        console.log(JSON.stringify({ selectedDate, county, address, category, description, number_of_participants, created_by }));
         const response = await fetch('http://152.94.160.72:3000/create-activity', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ time, county, address, category, description, number_of_participants, created_by }),
+            body: JSON.stringify({ time: selectedDate, county: selectedCounty, address, category: selectedCategory, description, number_of_participants, created_by }),
         });
     
         if (!response.ok) {
@@ -32,29 +62,49 @@ const NewActivity = () => {
     }
     
 
+
+
+    //{'Rogaland','Agder', 'Innland', 'Møre og Romsdal', 'Nordland', 'Oslo', 'Vestfold og Telemark', 'Troms of Finnmark', 'Trøndelag', 'Vestland', 'Viken'};
+
     return (
-        <View style= {styles.background}>
+        <View style={{flex: 1}}>
+            <ScrollView style= {styles.background}>
             <View style={styles.container}>
             
-            <TextInput
-                placeholder="Tidspunkt"
-                value={time}
-                onChangeText={setTime}
+            
+            <TouchableOpacity onPress={() => setShowDatePicker(!showDatePicker)}>
+                <Text style={{fontSize: 16}}>Dato og tidspunkt</Text>
+                <Text>
+                    {selectedDate}
+                </Text>
+                {showDatePicker && (
+                    <DatePicker
+                        date={selectedDate}
+                        onSelectedChange={setSelectedDate}
+                    />
+                )}
+            </TouchableOpacity>
+            
+            <Text></Text>
+            
+            <SelectList
+                 setSelected={setSelectedCounty}
+                 data={category}
+                 save="value"
+                 placeholder='Kategori'
             />
-            <TextInput
-                placeholder="Fylke (dropdown)"
-                value={county}
-                onChangeText={setCounty}
+            <Text></Text>
+
+            <SelectList
+                setSelected={setSelectedCategory}
+                data={county}
+                save="value"
+                placeholder='Fylke'
             />
             <TextInput
                 placeholder="Møtested"
                 value={address}
                 onChangeText={setAddress}
-            />
-            <TextInput
-                placeholder="Kategori (skal være dropdown)"
-                value={category}
-                onChangeText={setCategory}
             />
             <TextInput
                 placeholder="Beskrivelse"
@@ -73,11 +123,14 @@ const NewActivity = () => {
             />
             <TouchableOpacity style={styles.button} onPress={handleCreateActivity} >
                     <Text style={styles.buttonText}>Opprett ny aktivitet</Text>
-                    
+
                 </TouchableOpacity>
         
-
+            
+            
             </View>
+        
+        </ScrollView>
         <Footer />
         </View>
     );
@@ -93,6 +146,9 @@ const styles = StyleSheet.create({
         padding: 15,
         borderRadius: 10,
         margin: 20,
+        flex: 1,
+        paddingBottom: 50,
+    
       },
     button: {
         alignSelf: 'flex-start',
