5e15b42b3efcad9608d116a4fbe39ec4903bc9d2
MariErsland <257123@uis.no>
2023-03-25 15:04:40 +0100

    Made queue system

--- SupReactNative/components/inputValiation.tsx
+++ SupReactNative/components/inputValiation.tsx
@@ -9,7 +9,8 @@ export function validateInputLength(inputValue: string, MAX_LENGTH: number) {
 
 
 export function validateInputCharacters(inputValue: string) {
-    const DISALLOWED_CHARS = ['!', '#', '$', "'", '"','>', '<', ';', '-', '+', '=', '$', '€', '%', '&', '(', ')', '[', ']', '{', '}', '/', '\\' ]; 
+    const DISALLOWED_CHARS = 
+    ['!', '#', '$', "'", '"','>', '<', ';', '-', '+', '=', '$', '€', '%', '&', '(', ')', '[', ']', '{', '}', '/', '\\' ]; 
     if (DISALLOWED_CHARS.some(char => inputValue.includes(char))) {
     return `Input must not contain the following characters: ${DISALLOWED_CHARS.join(', ')}`;
   }
